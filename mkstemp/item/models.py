5219a66c73809d53e5881402aa0285e86e6f5136
avi <hi@avi.im>
2015-11-14 22:38:28 +0530

    Add `blank=True` to parent, title, text and url

--- mkstemp/item/models.py
+++ mkstemp/item/models.py
@@ -5,14 +5,14 @@ from mkstemp.utils import TimeStampMixin
 
 
 class Item(TimeStampMixin):
-    title = models.CharField(max_length=300, null=True)
-    text = models.TextField(null=True)
-    url = models.URLField(null=True)
+    title = models.CharField(max_length=300, null=True, blank=True)
+    text = models.TextField(null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
     item_type = models.IntegerField()
     locked = models.BooleanField()
     slug = models.SlugField(max_length=6)
 
-    parent = models.ForeignKey('self', null=True)
+    parent = models.ForeignKey('self', null=True, blank=True)
     author = models.ForeignKey(User)
 
     def __str__(self):
