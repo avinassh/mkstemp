d1585503f728f522916b5d7b91a4363727fd1930
avi <hi@avi.im>
2015-11-14 19:44:37 +0530

    Create model Item

--- mkstemp/item/models.py
+++ mkstemp/item/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from mkstemp.utils import TimeStampMixin
+
+
+class Item(TimeStampMixin):
+    title = models.CharField(max_length=300)
+    text = models.TextField()
+    url = models.URLField()
+    item_type = models.IntegerField()
+    locked = models.BooleanField()
+
+    parent = models.ForeignKey('self', null=True)
+    author = models.ForeignKey(User)
+
+    def __str__(self):
+        return "<{} - {}>".format(self.id, self.title[:30])
