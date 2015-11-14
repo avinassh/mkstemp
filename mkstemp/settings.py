3628d6cd3e98fd7b0472c1b7788869e56f574b27
avi <hi@avi.im>
2015-11-14 18:41:25 +0530

    Add mkstemp and also `item` to settings

--- mkstemp/settings.py
+++ mkstemp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mkstemp',
+    'mkstemp.item'
 ]
 
 MIDDLEWARE_CLASSES = [
