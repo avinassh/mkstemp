c5930dc78b56e7146521cd71e323c977d5ab39d3
avi <hi@avi.im>
2015-11-17 12:54:57 +0530

    Override `ItemForm.clean` to use `is_valid_story_or_comment`

--- mkstemp/item/forms.py
+++ mkstemp/item/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm
 
 from .models import Item
+from .utils import is_valid_story_or_comment
 
 
 class ItemForm(ModelForm):
@@ -9,3 +10,10 @@ class ItemForm(ModelForm):
         model = Item
         fields = ['title', 'text', 'url', 'parent']
         widgets = {'parent': forms.HiddenInput()}
+
+    def clean(self):
+        cleaned_data = super(ItemForm, self).clean()
+        status, msg = is_valid_story_or_comment(cleaned_data)
+        if not status:
+            raise forms.ValidationError(msg)
+        return cleaned_data
