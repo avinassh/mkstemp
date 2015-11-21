3cc7e746299f3cf5d415fdf247c32d3be45c956b
avi <hi@avi.im>
2015-11-21 21:39:14 +0530

    Update `Report` model
    - Add field resolution to report
    - Add field resolved_by to report
    - Alter field user on report, to add a `related_name`

--- mkstemp/reports/models.py
+++ mkstemp/reports/models.py
@@ -7,9 +7,12 @@ from mkstemp.utils import TimeStampMixin
 class Report(TimeStampMixin):
     text = models.TextField()
     resolved = models.BooleanField(default=False)
+    resolution = models.TextField(blank=True)
 
     item = models.ForeignKey('items.Item')
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, related_name='reports')
+    resolved_by = models.ForeignKey(
+        User, blank=True, null=True, related_name='resolved_reports')
 
     def __str__(self):
         return "{} - by {}".format(self.text[:30], self.user.username)
