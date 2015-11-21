ee9b85f63a83b3a6f5b02ac2b8e73cdba0675c91
avi <hi@avi.im>
2015-11-21 18:32:55 +0530

    Add Report model

--- mkstemp/reports/models.py
+++ mkstemp/reports/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from mkstemp.utils import TimeStampMixin
+
+
+class Report(TimeStampMixin):
+    text = models.TextField()
+    resolved = models.BooleanField(default=False)
+
+    item = models.ForeignKey('item.Item')
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return "{} - by {}".format(self.text[:30], self.user.username)
